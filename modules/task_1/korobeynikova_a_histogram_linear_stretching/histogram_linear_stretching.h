95a6e08a4025f31a637418cef879f6d666131347
navys00 <73278416+navys00@users.noreply.github.com>
2023-04-17 21:20:41 +0300

    second commit

--- modules/task_1/korobeynikova_a_histogram_linear_stretching/histogram_linear_stretching.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 Korobeynikova Alice
-#pragma once
-#include <vector>
-
-typedef unsigned char Color;
-typedef std::vector<Color> Image;
-
-void linearHistogramStretching(Image* image);
