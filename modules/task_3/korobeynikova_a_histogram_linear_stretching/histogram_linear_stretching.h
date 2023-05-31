a306d0fe131f0c1b33ff5da7b961af6e9469d599
Varyzgin <dima5varyzgin@gmail.com>
2023-05-31 20:23:39 +0300

    del what I dont need

--- modules/task_3/korobeynikova_a_histogram_linear_stretching/histogram_linear_stretching.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 Korobeynikova Alice
-#pragma once
-#include <vector>
-
-typedef unsigned char Color;
-typedef std::vector<Color> Image;
-
-void sequentialLinearHistogramStretching(Image* image_ptr);
-void parallelLinearHistogramStretching(Image* image_ptr);
