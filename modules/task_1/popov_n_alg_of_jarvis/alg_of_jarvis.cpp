eedb88a05ea99dcfdc279db48b220f3e246bd9ca
Ksatno <dartnikitos@gmail.com>
2023-05-31 22:07:37 +0300

    Version 2

---
+++ modules/task_1/popov_n_alg_of_jarvis/alg_of_jarvis.cpp
@@ -0,0 +1,39 @@
+// Copyright 2023 Popov Nikita
+#include "../../../modules/task_1/popov_n_alg_of_jarvis/alg_of_jarvis.h"
+
+int check_orientation(Point i, Point j, Point k) {
+  int value = (j.y - i.y) * (k.x - j.x) - (j.x - i.x) * (k.y - j.y);
+  if (value == 0) return 0;
+  // Коллинеарен
+  return (value > 0) ? 1 : 2;
+  // Ориентируется по-часовой стрелке, или против часовой стрелки
+}
+
+std::vector<Point> get_convex_body(const std::vector<Point>& p_arr) {
+  int size = p_arr.size();
+  if (size < 3) return std::vector<Point>();
+  std::vector<Point> convex_body;
+  int a = 0;
+  for (int i = 1; i < n; i++) {
+    if (p_arr[i].x < p_arr[a].x) {
+      a = i;
+    }
+  }
+  int change = a;
+  int b;
+
+  do {
+    convex_body.push_back(p_arr[change]);
+    b = (change + 1) % size;
+    for (int i = 0; i < size; i++) {
+      if (check_orientation(p_arr[change], p_arr[i], p_arr[b]) == 2) {
+        q = i;
+      }
+    }
+    change = b;
+  }
+
+  while (a != change);
+
+  return convex_body;
+}
---
+++ modules/task_1/popov_n_alg_of_jarvis/alg_of_jarvis.h
@@ -0,0 +1,33 @@
+// Copyright 2023 Popov Nikita
+#ifndef MODULES_TASK_1_POPOV_N_ALG_OF_JARVIS_ALG_OF_JARVIS_H_
+#define MODULES_TASK_1_POPOV_N_ALG_OF_JARVIS_ALG_OF_JARVIS_H_
+
+#include <algorithm>
+#include <atomic>
+#include <cassert>
+#include <iostream>
+#include <limits>
+#include <utility>
+#include <vector>
+
+struct Point {
+  int x = 0;
+  int y = 0;
+
+  Point() = default;
+  Point(int x, int y) : x(x), y(y) {}
+  bool operator>(const Point& second) const {
+    return std::make_pair(y, x) > std::make_pair(second.y, second.x);
+  }
+  bool operator<(const Point& second) const {
+    return std::make_pair(y, x) < std::make_pair(second.y, second.x);
+  }
+  bool operator==(const Point& second) const {
+    return y == second.y && x == second.x;
+  }
+  bool operator!=(const Point& second) const { return !(*this == second); }
+};
+int check_orientation(Point i, Point j, Point k);
+std::vector<Point> get_convex_body(const std::vector<Point>& p_arr);
+
+#endif  // MODULES_TASK_1_POPOV_N_ALG_OF_JARVIS_ALG_OF_JARVIS_H_
