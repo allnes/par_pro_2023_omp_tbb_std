07871126709bb575d1637bcd62bfe56c21df1e3d
mariablitkina <mariablit@mail.ru>
2023-05-31 21:32:57 +0300

    Task 4 fix 2

--- modules/task_4/blitkina_m_simpson/main.cpp
+++ modules/task_4/blitkina_m_simpson/main.cpp
@@ -5,7 +5,6 @@
 #include <vector>
 #include <functional>
 #include <utility>
-#include <chrono>
 #include "./simpson.h"
 
 TEST(Simson_Method_STD, Can_Integrate_One_Dim_Func) {
@@ -47,17 +46,6 @@ TEST(Simson_Method_STD, Can_Integrate_Two_Dim_Func_3) {
     double res = calcIntegral(scope, f);
     EXPECT_NEAR(res, 0.125, 0.01);
 }
-
-// TEST(Simson_Method_STD, Can_Integrate_Three_Dim_Func) {
-//     std::function<double(const std::vector<double>&)> f = [](const std::vector<double>& vec){
-//         return sin(vec[0]) * vec[1] * cos(vec[2]);};  // f(x,y,z) = sin(x) * y * cos(z)
-//     std::vector<std::pair<double, double>> scope = { {0, 1}, {0, 0.5}, {0, 1} };  // expected 0
-//     auto time1 = std::chrono::high_resolution_clock::now(); 
-//     double res = calcIntegral(scope, f);
-//     auto time2 = std::chrono::high_resolution_clock::now(); 
-//     std:: cout << "\nTime : " << std::chrono::duration_cast<std::chrono::duration<double>>(time2 - time1).count() << std::endl;
-//     EXPECT_NEAR(res, 0.0483528, 0.01);
-// }
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
